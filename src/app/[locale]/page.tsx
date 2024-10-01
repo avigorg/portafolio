0566a728018145adafdf1e244331c5127f1da5bf
Mauricio Gomez <avigorg77@hotmail.es>
2024-10-01 17:39:34 -0500

    feat: organizo las bird animation en una carpeta en components y creo nuevo componente de menu con el logo

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,12 @@
 import {useTranslations} from 'next-intl';
-import BirdsScene from '../components/BirdsScene';
+import BirdsScene from '../components/birds-animation/BirdsScene';
+import Menu from '../components/layout/Menu';
 
 export default function HomePage() {
   const t = useTranslations('HomePage');
   return  (
     <main>
+      <Menu />
       <BirdsScene />
     </main>
   );;
